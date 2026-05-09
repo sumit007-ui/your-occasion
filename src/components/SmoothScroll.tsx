3ad50d9378953e1984c8ea521da6abb455370c29
Sumit Kumar <sk78146083@gmail.com>
2026-05-09 19:35:41 +0530

    feat: implement custom SmoothScroll component using lenis library

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,11 +1,27 @@
 "use client";
 
-import { ReactLenis } from "@studio-freight/react-lenis";
+import { ReactNode, useEffect } from "react";
+import Lenis from "lenis";
 
-export function SmoothScroll({ children }: { children: React.ReactNode }) {
-  return (
-    <ReactLenis root options={{ lerp: 0.05, duration: 1.5, smoothWheel: true }}>
-      {children as any}
-    </ReactLenis>
-  );
+export function SmoothScroll({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.05,
+      duration: 1.5,
+      smoothWheel: true,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
 }
